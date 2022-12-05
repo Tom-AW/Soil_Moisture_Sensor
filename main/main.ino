c3f7957b4ba4dd66d6d95dc0f8e13babf645aa6a
Tom-AW <45534386+Tom-AW@users.noreply.github.com>
2022-12-05 22:17:56 +0000

    V1 code

--- main/main.ino
+++ main/main.ino
@@ -1,41 +1,48 @@
+#include <esp_wifi.h>
+
+// Constant declarations
+#define S_TO_US 1000000  // 1,000,000 uS in a second
+#define SleepInSeconds 3600 // 1 hour in seconds
+
+// Variable declarations
+float SoilMoistureLevel = 0;
+uint64_t timeToSleep = (uint64_t(S_TO_US) * uint64_t(SleepInSeconds));  // Set length of time to sleep for and cast both defines to uin64_t to prevent overflows
+
+// Pin declarations
+int SoilMoistureReadPin = 18;
+int WaterMeLedPin = 17;
+
 void setup()
 {
-  // Pin declarations
-  int SoilMoistureReadPin = 18;
-  int WaterMeLed = 17;
-
-  pinMode(WaterMeLed, output);  // Set GPIO pin 17 to a digital output
-
- // Variable declarations
-  float SoilMoistureLevel = 0;
-
-  // Constants
-  #define S_TO_US 1000000;  // 1,000,000 uS in a second
-  #define SleepInSeconds 3600;  // 1 hour in seconds
-
-  uint64_t timeToSleep = (S_TO_US * SleepInSeconds);  // Set length of time to sleep for
+  // Declare LED pin
+  pinMode(WaterMeLedPin, OUTPUT);  // Set GPIO pin 17 to a digital output
 
   // Disable Wireless functionality
   esp_wifi_stop();
 
   // Set sleep wakeup method
   esp_sleep_enable_timer_wakeup(timeToSleep);
-
-  // Start timer
-  int millis = 0;
 }
 
 void loop()
 {
   while (true)
   {
-    // If analog pin value below certain level turn on LED
-    if (SoilMoistureLevel < 0.5) {
-      WaterMeLed = HIGH;
+    //  Check soil moisture level from the analog pin value
+    SoilMoistureLevel = analogRead(SoilMoistureReadPin);
+
+    // If soil moisutre below certain level turn on LED
+    if (SoilMoistureLevel < 0.5)  // TODO: Find better threshold
+    {
+      digitalWrite(WaterMeLedPin,HIGH);
+    } else  // Else turn off LED
+    {
+      digitalWrite(WaterMeLedPin,LOW);
     }
 
-    // Light sleep for 1 hour
+    // Wake up in one hour
+    esp_light_sleep_start();
 
-    // TODO: How to update once water has been added other than hit reset button?
+    //TODO: Add an external interrupt to wake when moisture reaches certain level (e.g been watered)
   }
 }
